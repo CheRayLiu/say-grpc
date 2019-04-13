e293434d06a7041d0ba3cbcef84710d00e9fcc94
Ray Liu <rayliu109@gmail.com>
2019-04-13 19:28:43 -0400

    deployed on kubernetes

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,11 @@
 package main
 
-// import (
-// 	"log"
-// 	"os"
-// 	"os/exec"
-// )
-
 import (
 	"flag"
 	"fmt"
 	"net"
+	"io/ioutil"
+	"os/exec"
 
 	"google.golang.org/grpc"
 	"golang.org/x/net/context"
@@ -38,12 +34,23 @@ func main(){
 type server struct{}
 
 func (server) Say(ctx context.Context, text *pb.Text) (*pb.Speech, error){
-	return nil, fmt.Errorf("not implemented")
-}
+	f, err := ioutil.TempFile("","")
+	if err != nil{
+		return nil, fmt.Errorf("could not create temp file: %v", err)
+	}
 
-	// cmd := exec.Command("flite", "-t", os.Args[1], "-o", "output.wav")
-	// cmd.Stdout = os.Stdout
-	// cmd.Stderr = os.Stderr
-	// if err := cmd.Run(); err != nil{
-	// 	log.Fatal(err) 
-	// }
+	if err := f.Close(); err != nil {
+		return nil, fmt.Errorf("could not close temp file %s: %v", f.Name(), err)
+	}
+
+	cmd := exec.Command("flite", "-t", text.Text, "-o", f.Name())
+	if data, err := cmd.CombinedOutput(); err != nil{
+		return nil, fmt.Errorf("flite failed: %s", data) 
+	}
+
+	data, err := ioutil.ReadFile(f.Name())
+	if err != nil{
+		return nil, fmt.Errorf("could not read temp file %s: %v", f.Name(), err) 
+	}
+	return &pb.Speech{Audio: data}, nil
+}
--- say/main.go
+++ say/main.go
@@ -1,23 +1,33 @@
-package mian
+package main
 
 import (
 	"flag"
 	"log"
-	"net"
+	"google.golang.org/grpc"
+	"io/ioutil"
+	"context"
+	"os"
+	"fmt"
+	
 	pb "github.com/CheRayLiu/say-grpc/api"
 )
 func main(){
 	backend := flag.String("b", "localhost:8080", "address of the say backend")
 	flag.Parse()
+
+	if flag.NArg() < 1{
+		fmt.Printf("Usage:\n\t%s \"text to speech\"\n", os.Args[0])
+		os.Exit(1)
+	}
 	output := flag.String("o", "output.wav", "wav file where the output will be written")
-	conn, err := grpc.Dial(*backend)
+	conn, err := grpc.Dial(*backend, grpc.WithInsecure())
 	if err != nil{
 		log.Fatalf("could not connection to %s: %v", *backend, err )
 	}
-	defer conn.close()
+	defer conn.Close()
 
-	client := pb.NewTextSpeechClient(conn)
-	text : = &pb.Text{Text: "hello, there"}
+	client := pb.NewTextToSpeechClient(conn)
+	text := &pb.Text{Text: flag.Arg(0)}
 	res, err := client.Say(context.Background(), text)
 	if err != nil {
 		log.Fatalf("could not say %s: %v", text.Text, err)
